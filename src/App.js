f319af0adf7fc0c650e0a4290a20e0b9dadc0d63
Hesham <hisham.mosa.910@gmail.com>
2024-05-07 23:47:37 +0300

    new task 07

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+ //import Form from './form/Login';
+import RegistrationForm from './form/signup';
+function App() {
+  return (
+     <div className="App">
+      
+      {/* <Form/>  */}
+    <RegistrationForm/>
+    </div>
+  );
+}
+
+export default App;
